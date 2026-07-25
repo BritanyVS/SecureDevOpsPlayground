da11336146e4eeeea08b875214ae5134180101cf
Britany Villalobos <britanyvsalazar@gmail.com>
2026-07-25 17:08:23 -0600

    Cree las relaciones de las entidades

--- SecureDevOps.API/Data/AppDbContext.cs
+++ SecureDevOps.API/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using SecureDevOps.API.Models;
 
 namespace SecureDevOps.API.Data;
 
@@ -6,5 +7,34 @@ public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
+    }
+
+    public DbSet<User> Users => Set<User>();
+    public DbSet<TaskItem> TaskItems => Set<TaskItem>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // User
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasIndex(u => u.Username).IsUnique();
+            entity.HasIndex(u => u.Email).IsUnique();
+        });
+
+        // TaskItem
+        modelBuilder.Entity<TaskItem>(entity =>
+        {
+            entity.HasOne(t => t.CreatedByUser)
+                .WithMany(u => u.CreatedTasks)
+                .HasForeignKey(t => t.CreatedByUserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            entity.HasOne(t => t.AssignedToUser)
+                .WithMany(u => u.AssignedTasks)
+                .HasForeignKey(t => t.AssignedToUserId)
+                .OnDelete(DeleteBehavior.Restrict);
+        });
     }
 }
