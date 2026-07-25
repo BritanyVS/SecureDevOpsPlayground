91c8b63f3242530371ec2bf626b5504e563aab89
Britany Villalobos <britanyvsalazar@gmail.com>
2026-07-25 16:52:03 -0600

    Configuración del ORM Entity Framework Core con Sql Server

--- SecureDevOps.API/Program.cs
+++ SecureDevOps.API/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using SecureDevOps.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
