c1d97d2c24891ed416541b9e56a0b106a7afbddc
Britany Villalobos <britanyvsalazar@gmail.com>
2026-07-25 19:24:49 -0600

    Implementación de hasheo de contraseñas mediante BCrypt

---
+++ SecureDevOps.API/Services/IPasswordHasherService.cs
@@ -0,0 +1,7 @@
+namespace SecureDevOps.API.Services;
+
+public interface IPasswordHasherService
+{
+    string HashPassword(string password);
+    bool VerifyPassword(string password, string passwordHash);
+}
